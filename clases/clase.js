049a609c2dd6686c8a739cf3e938644b903bedb5
agusgonzalezc <agusgonzalez@itba.edu.ar>
2023-05-26 16:38:55 -0300

    commit entrega java

---
+++ clases/clase.js
@@ -0,0 +1,22 @@
+let array1=[1,2,3]
+console.log(array1)
+console.log(array1[0])
+
+//metodos de array
+//POP: elimina el ultimo elemento
+array1.pop()
+
+//PUSH: añade un elemento al array
+array1.push(3.14)
+console.log(array1)
+
+//SHIFT: elimina el primer elemento del array
+//UNSHIFT: agrega un elemento al principio del array
+console.log(array1)
+
+//undefined: valor que no esta definido
+
+//indexof:
+console.log(array1.indexOf(3.14))
+
+//Reverse: devuelve el array en reversa (destruye el array original)
